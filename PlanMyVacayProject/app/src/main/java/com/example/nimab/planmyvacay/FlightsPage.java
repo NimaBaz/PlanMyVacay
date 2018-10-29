ab8939ec4fd2d7cb86a4ee00754c31062873dba6
nibazoft <nibazoft@ucsc.edu>
2018-10-28 18:57:01 -0700

    FLight page and Road Trips page

---
+++ PlanMyVacayProject/app/src/main/java/com/example/nimab/planmyvacay/FlightsPage.java
@@ -0,0 +1,38 @@
+package com.example.nimab.planmyvacay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FlightsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flights_page);
+    }
+
+    public void Trip(View view) {
+    }
+
+    public void List(View view) {
+    }
+
+    public void Flight(View view) {
+    }
+
+    public void Rental(View view) {
+    }
+
+    public void Hotel(View view) {
+    }
+
+    public void Excursion(View view) {
+    }
+
+    public void Restaurant(View view) {
+    }
+
+    public void Destination(View view) {
+    }
+}
---
+++ PlanMyVacayProject/app/src/main/java/com/example/nimab/planmyvacay/RoadTripsPage.java
@@ -0,0 +1,13 @@
+package com.example.nimab.planmyvacay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoadTripsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_road_trips_page);
+    }
+}
